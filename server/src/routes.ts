e16132a295a823898352d5e92c6ae61a50c734f1
jmalish <jordanmalish@gmail.com>
2019-04-12 23:28:46 -0400

    Fix issue with alias array not being instantiated, fix issue with not checking if regmatch is null

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -2,6 +2,7 @@ import * as express from 'express';
 import * as fs from 'fs';
 
 let router = express.Router();
+
 let fileName = 'dcslog.json';
 
 
@@ -23,15 +24,14 @@ function getPilots(callback) {
     let pilots: Pilot[] = [];
     const regexp = new RegExp('player: (.+) side:. slot:.+ ucid: ([a-z0-9]{32})'); // create regex
 
-    let logFile = JSON.parse(fs.readFileSync(fileName)).results[0].series[0].values;
+    let logFile = JSON.parse(fs.readFileSync(fileName)).results[0].series[0].values; // ignore buffer warning
 
     logFile.forEach(line => {
         if (line[1].indexOf('ALLOW') > 0) { // check if line is a pilot entering a plane
-
-            const regMatch = line[1].match(regexp); // get the regex match, which includes the groups
             try {
+                const regMatch = line[1].match(regexp); // get the regex match, which includes the groups
 
-                if (regMatch[2] && regMatch) { // apparently it's possible for a pilot to not have a name, if that happens we'll just ignore the account
+                if (regMatch !== null) { // make sure regmatch is not null, pretty sure this happens when the pilots name is blank
                     const newPilot = new Pilot(regMatch[1], regMatch[2]); // create temp pilot object
 
                     const testForExistingPilot = pilots.find(pilot => { // search through existing pilots
@@ -41,10 +41,13 @@ function getPilots(callback) {
                     if (testForExistingPilot) { // if this is not null, the pilot already exists
                         if (testForExistingPilot.name !== newPilot.name) { // if the two names don't match, we add an alias
                             // testForExistingPilot now equals an existing pilot in the array
-                            const testAliases = testForExistingPilot.aliases.find(alias => alias === newPilot.name);
 
-                            if (!testAliases) { // if the returned array is empty, the name does not exist in aliases yet
-                                testForExistingPilot.aliases.push(newPilot.name);
+                            if (testForExistingPilot.aliases.length > 0) { // if this pilot doesn't have any aliases, no need to check for a match
+                                if (testForExistingPilot.aliases.find(alias => alias !== newPilot.name)) {
+                                    testForExistingPilot.addAlias(newPilot.name);
+                                }
+                            } else {
+                                testForExistingPilot.addAlias(newPilot.name);
                             }
                         } // otherwise do nothing
                     } else { // else pilot does not exist, so we should add it
@@ -52,7 +55,6 @@ function getPilots(callback) {
                     }
                 }
             } catch (err) {
-                console.log(regMatch);
                 console.log(err);
             }
         }
@@ -64,7 +66,7 @@ function getPilots(callback) {
 class Pilot {
     name: string;
     ucid: string;
-    aliases: string[];
+    aliases: string[] = [];
 
     constructor(_name, _ucid) {
         this.name = _name;
